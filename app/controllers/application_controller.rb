cd3b3e3f0a48e6dc36c94956cc2ec0573aeb25aa
4723122h <4723122h@hgu.jp>
2025-07-30 04:43:13 +0000

    hello world

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   
   def hello
-    render html: "hello world!"
+    render html: "hello world!!!!"
   end
+
+  def hello_2
+    render html: "H W 2"
+  end
+
 end
